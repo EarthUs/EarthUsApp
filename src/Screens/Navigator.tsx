a4820724681eaf36d39319d68ec4a08f6c45a6e9
hyeminnoh <shrmffkfk@naver.com>
2020-06-11 03:06:10 +0900

    [Update] Create Weather, Statistic, Walking Tab

--- src/Screens/Navigator.tsx
+++ src/Screens/Navigator.tsx
@@ -1,15 +1,22 @@
 import React, {useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {UserContext} from '~/Context/User';
 
 import Loading from '~/Screens/Loading';
 
-import Login from'~/Screens/Login';
-import Home from'~/Screens/Home';
+import Login from '~/Screens/Login';
+import Home from '~/Screens/Home';
+import Weather from '~/Screens/Weather';
+import Statistics from '~/Screens/Statistics';
+import Walking from '~/Screens/Walking';
 
 const Stack = createStackNavigator();
+const BottomTab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 const LoginNavigator = () => {
   return (
@@ -30,25 +37,117 @@ const LoginNavigator = () => {
   );
 };
 
-const HomeNavigator = () => {
+// 각 tab
+const HomeTab = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="Home"
         component={Home}
         options={{
-          title: 'Home',
-          headerTintColor: '#E70915',
+          title: 'EarthUs',
           headerStyle: {
-            backgroundColor: '#141414',
-            borderBottomWidth: 0,
+            backgroundColor: '#f85b00',
           },
+          headerTintColor: '#ffffff',
           headerTitleStyle: {
             fontWeight: 'bold',
           },
         }}
       />
     </Stack.Navigator>
+  );
+};
+
+const WeatherTab = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Weather"
+        component={Weather}
+        options={{
+          title: 'EarthUs',
+          headerStyle: {
+            backgroundColor: '#007fff',
+          },
+          headerTintColor: '#ffffff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const StatisticsTab = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Statistics"
+        component={Statistics}
+        options={{
+          title: 'EarthUs',
+          headerStyle: {
+            backgroundColor: '#772fa3',
+          },
+          headerTintColor: '#ffffff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const WalkingTab = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Walking"
+        component={Walking}
+        options={{
+          title: 'EarthUs',
+          headerStyle: {
+            backgroundColor: '#5ba800',
+          },
+          headerTintColor: '#ffffff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const MainTabs = () => {
+  return (
+    <BottomTab.Navigator
+      tabBarOptions={{
+        showLabel: true,
+        inactiveBackgroundColor: '#ffffff',
+        activeBackgroundColor: '#ffffff',
+        activeTintColor: '#212121',
+      }}>
+      <BottomTab.Screen name="Home" component={HomeTab} />
+      <BottomTab.Screen name="Weather" component={WeatherTab} />
+      <BottomTab.Screen name="Statistics" component={StatisticsTab} />
+      <BottomTab.Screen name="Walking" component={WalkingTab} />
+    </BottomTab.Navigator>
+  );
+};
+
+const MainNavigator = () => {
+  return (
+    <Drawer.Navigator
+      drawerPosition="right"
+      drawerType="slide"
+      // drawerContent={(props) => <CustomDrawer props={props} />}
+    >
+      <Drawer.Screen name="MainTabs" component={MainTabs} />
+    </Drawer.Navigator>
   );
 };
 
@@ -61,7 +160,7 @@ export default () => {
   }
   return (
     <NavigationContainer>
-      {userInfo ? <HomeNavigator /> : <LoginNavigator />}
+      {userInfo ? <MainNavigator /> : <LoginNavigator />}
     </NavigationContainer>
   );
 };
---
+++ src/Screens/Weather/index.tsx
@@ -0,0 +1,60 @@
+import React, {useContext, useLayoutEffect, useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import {StackNavigationProp} from '@react-navigation/stack';
+import Styled from 'styled-components/native';
+
+import {UserContext} from '~/Context/User';
+import {Text} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+const Container = Styled.ScrollView`
+  flex: 1;
+  background-color: transparent;
+`;
+
+const StyleButton = Styled.TouchableOpacity`
+  padding: 8px;
+`;
+const Icon = Styled.Image`
+`;
+
+type NavigationProp = StackNavigationProp<MainNaviParamList, 'Home'>;
+interface Props {
+  navigation: NavigationProp;
+}
+
+const Home = ({navigation}: Props) => {
+  const {logout} = useContext<IUserContext>(UserContext);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <StyleButton
+          onPress={() => {
+            logout();
+          }}>
+          <Icon source={require('~/Assets/Images/ic_logout.png')} />
+        </StyleButton>
+      ),
+    });
+  }, [logout, navigation]);
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <LinearGradient
+      colors={['#007fff', '#ffffff', '#ffffff']}
+      style={{
+        alignItems: 'center',
+        height: 900,
+      }}>
+      <Container>
+        <Text>Hi</Text>
+      </Container>
+    </LinearGradient>
+  );
+};
+
+export default Home;
